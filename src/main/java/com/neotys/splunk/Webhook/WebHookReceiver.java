5ed955cd16b8a169aed62a7539b4fca7e2c6f13d
Thomas Théol <thomas.theol@neotys.com>
2021-05-18 12:32:54 +0200

    Add the ability to test the webhook connection from NeoLoad Web

--- src/main/java/com/neotys/splunk/Webhook/WebHookReceiver.java
+++ src/main/java/com/neotys/splunk/Webhook/WebHookReceiver.java
@@ -16,27 +16,25 @@ import java.util.List;
 
 import static com.neotys.splunk.conf.Constants.*;
 
-public class WebHookReceiver  extends AbstractVerticle {
+public class WebHookReceiver extends AbstractVerticle {
     private HttpServer server;
     private NeoLoadLogger loadLogger;
     int httpPort;
     private Vertx rxvertx;
-    List<String> testidlist=new ArrayList<>();
+    List<String> testidlist = new ArrayList<>();
+
     public void start() {
 
-        loadLogger=new NeoLoadLogger(this.getClass().getName());
+        loadLogger = new NeoLoadLogger(this.getClass().getName());
         Router router = Router.router(vertx);
         router.route().handler(BodyHandler.create());
         router.post(WEBHOOKPATH).handler(this::postWebHook);
         router.get(HEALTH_PATH).handler(this::getHealth);
-        String port=System.getenv(SECRET_PORT);
-        if(port==null)
-        {
-            httpPort=HTTP_PORT;
-        }
-        else
-        {
-            httpPort=Integer.parseInt(port);
+        String port = System.getenv(SECRET_PORT);
+        if (port == null) {
+            httpPort = HTTP_PORT;
+        } else {
+            httpPort = Integer.parseInt(port);
         }
         server = vertx.createHttpServer();
 
@@ -49,50 +47,53 @@ public class WebHookReceiver  extends AbstractVerticle {
     }
 
     private void postWebHook(RoutingContext routingContext) {
-        if(routingContext.getBody()==null) {
-            loadLogger.error("Technical error - there is no payload" );
+        if (routingContext.getBody() == null) {
+            loadLogger.error("Technical error - there is no payload");
             routingContext.response().setStatusCode(500).end("Technical error - there is no payload");
             return;
         }
-        JsonObject body=routingContext.getBodyAsJson();
-        if(body.containsKey(TESTID_KEY))
-        {
-            String testid=body.getString(TESTID_KEY);
-            if(!testidlist.contains(testid))
-            {
-                testidlist.add(testid);
+        JsonObject body = routingContext.getBodyAsJson();
+        if (body.containsKey(TESTID_KEY)) {
+            String testid = body.getString(TESTID_KEY);
+            if (!testidlist.contains(testid)) {
                 loadLogger.setTestid(testid);
-                loadLogger.info("Received Webhook with testid  " + testid);
-                try {
-                    NeoLoadHttpHandler httpHandler = new NeoLoadHttpHandler(testid);
-                    Future<Boolean> booleanFuture = httpHandler.sync(vertx);
-                    booleanFuture.setHandler(booleanAsyncResult -> {
-                        if (booleanAsyncResult.succeeded()) {
-                            testidlist.remove(testid);
-                            loadLogger.info("Test "+testid +" sent properly to splunk");
-                            routingContext.response().setStatusCode(200)
-                                    .end("Results sent to Splunk");
-                        } else {
-                            routingContext.response().setStatusCode(500)
-                                    .end("Issue to sent to Splunk");
-                        }
-                    });
+                loadLogger.info("Received Webhook with test id  " + testid);
 
-                } catch (Exception e) {
-                    loadLogger.error("Technical error " + e.getMessage());
-                    e.printStackTrace();
-                    routingContext.response().setStatusCode(500).end(e.getMessage());
+                //Handle the test payload
+                if (testid.equals("$(test_result_id)")) {
+                    routingContext.response().setStatusCode(200).end("Payload is correct");
+                    loadLogger.info("NeoLoad webhook correctly configured from host " + routingContext.request().remoteAddress().host());
+                } else {
+                    testidlist.add(testid);
+
+                    //Send the test metrics to splunk
+                    try {
+                        NeoLoadHttpHandler httpHandler = new NeoLoadHttpHandler(testid);
+                        Future<Boolean> booleanFuture = httpHandler.sync(vertx);
+                        booleanFuture.setHandler(booleanAsyncResult -> {
+                            if (booleanAsyncResult.succeeded()) {
+                                testidlist.remove(testid);
+                                loadLogger.info("Test " + testid + " sent properly to splunk");
+                                routingContext.response().setStatusCode(200)
+                                        .end("Results sent to Splunk");
+                            } else {
+                                routingContext.response().setStatusCode(500)
+                                        .end("Issue sending to Splunk");
+                            }
+                        });
+
+                    } catch (Exception e) {
+                        loadLogger.error("Technical error " + e.getMessage());
+                        e.printStackTrace();
+                        routingContext.response().setStatusCode(500).end(e.getMessage());
+                    }
                 }
-            }
-            else
-            {
+            } else {
                 routingContext.response().setStatusCode(302)
                         .end("Results is already synchronised with Splunk");
             }
 
-        }
-        else
-        {
+        } else {
             routingContext.response()
                     .setStatusCode(420)
                     .end("Response time does not have any TestID");
